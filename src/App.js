54758b3ae2d9b7b260017fd6a7ced4b7ae359f5b
Ridoy Roy <ridoyroycse53engg@gmail.com>
2022-04-17 22:14:15 +0600

    Header style added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import Header from './components/Header/Header';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import { Route, Routes } from 'react-router-dom';
 import Home from './components/Home/Home';
 import Login from './components/Login/Login';
@@ -8,7 +7,7 @@ import Register from './components/Login/Register';
 function App() {
   return (
     <div >
-      < h1 > Welcome to travel with Roy</h1 >
+
       <Header></Header>
       <Routes>
         <Route path='/' element={<Home></Home>}></Route>
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
+import './Header.css'
 const Header = () => {
     return (
-        <div className='header'>
+        <div className='header container'>
             <div>
                 <h3>Travel With Roy</h3>
             </div>
