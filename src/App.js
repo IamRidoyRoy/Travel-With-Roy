87657f500e41f0a66228152d566114aa41d904d3
Ridoy Roy <ridoyroycse53engg@gmail.com>
2022-04-18 15:19:15 +0600

    Page not found page added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import './App.css';
 import Header from './components/Header/Header';
 import { Route, Routes } from 'react-router-dom';
 import Home from './components/Home/Home';
-import Login from './components/Login/Login';
 import Register from './components/Login/Register';
 import Checkout from './components/Checkout/Checkout';
 import Footer from './components/Footer/Footer';
 import Blogs from './components/Blogs/Blogs';
 import About from './components/About/About';
+import PageNotFound from './components/PageNotFound/PageNotFound';
 function App() {
   return (
     <div >
@@ -19,6 +19,7 @@ function App() {
         <Route path='/about' element={<About></About>}></Route>
         <Route path='/register' element={<Register></Register>}></Route>
         <Route path='/checkout' element={<Checkout></Checkout>}></Route>
+        <Route path='/*' element={<PageNotFound></PageNotFound>} ></Route>
 
       </Routes>
       <Footer></Footer>
---
+++ src/components/PageNotFound/PageNotFound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const PageNotFound = () => {
+    return (
+        <div className='text-center mt-5'>
+            <h2>The page you are looking for is not found!</h2>
+            <h1>404 Not found!</h1>
+        </div>
+    );
+};
+
+export default PageNotFound;
