fbf14e18c0b60c8158e077296076d09b4c29e055
Ridoy Roy <ridoyroycse53engg@gmail.com>
2022-04-18 13:42:17 +0600

    Footer added

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from './components/Home/Home';
 import Login from './components/Login/Login';
 import Register from './components/Login/Register';
 import Checkout from './components/Checkout/Checkout';
+import Footer from './components/Footer/Footer';
 function App() {
   return (
     <div >
@@ -17,6 +18,7 @@ function App() {
         <Route path='/checkout' element={<Checkout></Checkout>}></Route>
 
       </Routes>
+      <Footer></Footer>
     </div >
   );
 }
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Footer.css'
+const Footer = () => {
+    return (
+        <div className='footer'>
+            <div>
+                <h1>Travel With Roy</h1>
+            </div>
+            <div className='contact'>
+                <h3>Contact Me</h3>
+                <p>Email: ridoyroycse53engg@gmail.com</p>
+                <p>Address: Dhaka, Bangladesh</p>
+            </div>
+        </div>
+    );
+};
+
+export default Footer;
